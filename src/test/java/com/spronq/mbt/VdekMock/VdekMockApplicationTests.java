cef9d7291b370bc15b7b38501a72d10e74b01025
ikbened <ed.hilkman@spronq.com>
2018-04-29 17:13:40 +0200

    For all tests made an init method for default extended shipment
    How to create an constructor when class has @NoArgsConstructor and @AllArgsConstructor?

--- src/test/java/com/spronq/mbt/VdekMock/VdekMockApplicationTests.java
+++ src/test/java/com/spronq/mbt/VdekMock/VdekMockApplicationTests.java
@@ -26,20 +26,22 @@ public class VdekMockApplicationTests {
     @Autowired
     ExtendedShipmentRepository shipmentRepository;
 
+    private JSONObject extShipment;
+
     @Before
     public void initPath() {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = 8080;
     }
 
-    @Test
-    public void PostSimpleShipment() {
+    @Before
+    public void initShipment() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyymmddhhmmss");
         String d = sdf.format(new Date());
 
-        JSONObject extShipment = new JSONObject();
         try {
-                extShipment
+            extShipment = new JSONObject();
+            extShipment
                     .put("Customernumber", "1718")
                     .put("EAN", "9789034506801")
                     .put("OrderId", "1718_" + d)
@@ -70,6 +72,10 @@ public class VdekMockApplicationTests {
             //some exception handler code.
         }
 
+    }
+
+    @Test
+    public void PostSimpleShipment() {
 
         given()
 				.contentType("application/json")
@@ -83,41 +89,8 @@ public class VdekMockApplicationTests {
 
     @Test
     public void ShipmentWithoutCustomerNumber() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyymmddhhmmss");
-        String d = sdf.format(new Date());
-
-        JSONObject extShipment = new JSONObject();
-        try {
-            extShipment
-                    .put("Customernumber", "")
-                    .put("EAN", "9789034506801")
-                    .put("OrderId", "1718_" + d)
-                    .put("OrderLine", "1")
-                    .put("SchoolId", "1641")
-                    .put("SessionId", "")
-                    .put("EmailAddress", "cust" + d + "@mailinator.com")
-                    .put("Label", "VDE")
-                    .put("PostalCode", "2323ab")
-                    .put("FirstName", "Bokito")
-                    .put("MiddleName", "de")
-                    .put("LastName", "Aap")
-                    .put("GroupName", "")
-                    .put("Administration", "Dynamics")
-                    .put("Address", "SomeStreet")
-                    .put("AddressNumber", "1")
-                    .put("AddressAdjunct", "")
-                    .put("City", "SomeCity")
-                    .put("Country", "SomeCountry")
-                    .put("Gender", "M")
-                    .put("BirthDate", "2004-02-01")
-                    .put("Amount", "1")
-                    .put("StartDate", "2018-04-27")
-                    .put("DisplayName", "SomeDisplayName")
-                    .put("EmailUser", "user" + d + "@mailinator.com");
-
-        } catch (JSONException e) {
-            //some exception handler code.
-        }
+        extShipment.remove("Customernumber");
+        //What about CustomerNumber = "" and CustomerNumber = "NULL"??
 
         Response response =
                 given()
