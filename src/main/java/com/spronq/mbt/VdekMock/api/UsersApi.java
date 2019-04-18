74552fbd1334753c14b7c662d8bd75769a2cafe2
ikbened <ed.hilkman@spronq.com>
2019-04-18 12:19:56 +0200

    Added logging for users

--- src/main/java/com/spronq/mbt/VdekMock/api/UsersApi.java
+++ src/main/java/com/spronq/mbt/VdekMock/api/UsersApi.java
@@ -2,6 +2,7 @@ package com.spronq.mbt.VdekMock.api;
 
 import com.spronq.mbt.VdekMock.model.User;
 import com.spronq.mbt.VdekMock.repository.UsersRepository;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,11 +13,14 @@ import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @RestController
 @RequestMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UsersApi {
 
     private UsersRepository repository;
+    private static final Logger LOG = getLogger(UsersApi.class);
 
     @Autowired
     public UsersApi(UsersRepository repository) {
@@ -25,6 +29,7 @@ public class UsersApi {
 
     @GetMapping("/{id}")
     public Mono<ResponseEntity<User>> getUserById(@PathVariable(value = "id") String id) {
+        LOG.debug("getUserById");
         return repository.findById(id)
                 .map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build());
@@ -32,12 +37,14 @@ public class UsersApi {
 
     @GetMapping(params = "email")
     public Flux<User> getUsersByEmail(@RequestParam(value = "email") String email) {
+        LOG.debug("getUsersByEmail");
         return repository.findAllByEmail(email);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.ACCEPTED)
     public Mono<User> createUser(@Valid @RequestBody User user) {
+        LOG.debug("createUser");
         return repository.save(user);
     }
 
